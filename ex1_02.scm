622217931638201a9da75ef803c131c89ab25bc0
Larry Marburger <larry@marburger.cc>
2010-07-04 21:03:49 -0400

    Move test code from 1.2 out of the exercises.

--- ex1_02.scm
+++ ex1_02.scm
@@ -111,26 +111,6 @@
 (define (k n) (* 5 n n))
 
 
-(define (fib n)
-  (cond ((= n 0) 0)
-        ((= n 1) 1)
-        (else (+ (fib (- n 1))
-                 (fib (- n 2))))))
-
-(define (fib n)
-  (fib-iter 1 0 n))
-
-; (fib-iter 1 0 5)
-; (fib-iter 1 1 4)
-; (fib-iter 2 1 3)
-; (fib-iter 3 2 2)
-; (fib-iter 5 3 1)
-; (fib-iter 8 5 0)
-(define (fib-iter a b count)
-  (if (= count 0)
-    b
-    (fib-iter (+ a b) a (- count 1))))
-
 
 ; Exercise 1.11
 ; Exercise 1.12
---
+++ ex1_02_test.scm
@@ -0,0 +1,55 @@
+; Section 1.2.2
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+; (fib-iter 1 0 5)
+; (fib-iter 1 1 4)
+; (fib-iter 2 1 3)
+; (fib-iter 3 2 2)
+; (fib-iter 5 3 1)
+; (fib-iter 8 5 0)
+(define (fib-iter a b count)
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 100)
+; (cc 100 5)
+; (+ (cc 100 4) (cc 50 5)
+; (+ (cc 100 4) (+ (cc 50 4) (cc 0 5))
+; (+ (cc 100 4) (+ (cc 50 4) 1)
+; (+ (cc 100 4) (+ (+ (cc 50 3) (cc 25 4)) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (cc 40 3)) 1) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (+ (cc 40 2) (cc 30 3))) 1) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (+ (cc 40 2) (+ (cc 30 2) (cc 20 3)))) 1) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (+ (cc 40 2) (+ (cc 30 2) (+ (cc 20 2) (cc 10 3))))) 1) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (+ (cc 40 2) (+ (cc 30 2) (+ (cc 20 2) (+ (cc 10 2) (cc 0 3)))))) 1) 1)
+; (+ (cc 100 4) (+ (+ (+ (cc 50 2) (+ (cc 40 2) (+ (cc 30 2) (+ (cc 20 2) (+ (cc 10 2) 1))))) 1) 1)
+; lots more
